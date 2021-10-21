990a74d94f86ea58a59cae018fbea38a2dd66f9e
antonchuvirau <anton_chuvirau@icloud.com>
2021-10-21 19:45:18 +0300

    Git init

---
+++ app/js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    const helpSectionSwiperElement = document.querySelector(`.help-swiper`);
+    const healthSectionSwiperElement = document.querySelector(`.health-swiper`);
+    const mediaSwiperInstanceCollection = document.querySelectorAll(`.media-swiper__instance`);
+    
+    if (healthSectionSwiperElement) {
+        new Swiper(healthSectionSwiperElement, {
+            slidesPerView: `auto`,
+            spaceBetween: 55,
+            loop: false,
+            slidesOffsetBefore: (window.innerWidth - 1170) / 2,
+            navigation: {
+                prevEl: healthSectionSwiperElement.closest(`.help-section`).querySelector(`.carousel-navigation__btn-prev`),
+                nextEl: healthSectionSwiperElement.closest(`.help-section`).querySelector(`.carousel-navigation__btn-next`)
+            }
+        });
+    }
+    if (helpSectionSwiperElement) {
+        new Swiper(helpSectionSwiperElement, {
+            slidesPerView: `auto`,
+            spaceBetween: 55,
+            loop: false,
+            slidesOffsetBefore: (window.innerWidth - 1170) / 2,
+            navigation: {
+                prevEl: helpSectionSwiperElement.closest(`.help-section`).querySelector(`.carousel-navigation__btn-prev`),
+                nextEl: helpSectionSwiperElement.closest(`.help-section`).querySelector(`.carousel-navigation__btn-next`)
+            }
+        });
+    }
+    if (mediaSwiperInstanceCollection.length) {
+        for (const mediaSwiperInstance of mediaSwiperInstanceCollection) {
+            new Swiper(mediaSwiperInstance, {
+                effect: 'fade',
+                fadeEffect: {
+                    crossFade: true
+                },
+                speed: 800,
+                navigation: {
+                    prevEl: mediaSwiperInstance.closest(`.text-section`).querySelector(`.carousel-navigation__btn-prev`),
+                    nextEl: mediaSwiperInstance.closest(`.text-section`).querySelector(`.carousel-navigation__btn-next`)
+                }
+            });
+        }
+    }
+});
